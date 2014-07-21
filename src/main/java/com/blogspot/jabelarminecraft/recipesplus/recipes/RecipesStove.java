2f4d3484ad95800d4525617b49993168d03a5e02
jabelar <julian_abelar@hotmail.com>
2014-07-20 19:03:04 -0700

    add copyright notices

--- src/main/java/com/blogspot/jabelarminecraft/recipesplus/recipes/RecipesStove.java
+++ src/main/java/com/blogspot/jabelarminecraft/recipesplus/recipes/RecipesStove.java
@@ -1,9 +1,26 @@
+/**
+    Copyright (C) 2014 by jabelar
+
+    This file is part of jabelar's Minecraft Forge modding examples; as such,
+    you can redistribute it and/or modify it under the terms of the GNU
+    General Public License as published by the Free Software Foundation,
+    either version 3 of the License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU General Public License for more details.
+
+    For a copy of the GNU General Public License see <http://www.gnu.org/licenses/>.
+*/
+
 package com.blogspot.jabelarminecraft.recipesplus.recipes;
 
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 import java.util.Map.Entry;
+
 import net.minecraft.block.Block;
 import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
@@ -17,8 +34,8 @@ public class RecipesStove
     /**
      * The list of cooking results.
      */
-    private Map cookingList = new HashMap();
-    private Map experienceList = new HashMap();
+    private final Map cookingList = new HashMap();
+    private final Map experienceList = new HashMap();
     private static final String __OBFID = "CL_00000085";
 
     /**
